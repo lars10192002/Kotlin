b42645b31aa147fb1926cfd274f6308895a995cb
lucakao <lucakao@softchef.com>
2022-06-05 22:39:31 +0800

    [6.5] modify base module , java to kotlin

--- base/src/main/java/com/example/core/http/HttpClient.kt
+++ base/src/main/java/com/example/core/http/HttpClient.kt
@@ -29,7 +29,7 @@ object HttpClient : OkHttpClient() {
 
             override fun onResponse(call: Call, response: Response) {
                 when (response.code()) {
-                    in 200..299 -> { entityCallback.onSuccess(convert<Any>(response.body()!!.string()!!, type) as T) }
+                    in 200..299 -> { entityCallback.onSuccess(convert(response.body()!!.string()!!, type) as T) }
                     in 400..499 -> { entityCallback.onFailure("客户端错误") }
                     in 500..599 -> { entityCallback.onFailure("服务器错误") }
                     else -> { entityCallback.onFailure("未知错误") }
